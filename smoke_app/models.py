182fa434c0530fed3c2d8bdf67812139bba431fc
Mohammad Yasein <myn.shanabli@gmail.com>
2020-12-22 18:45:28 +0200

    create ORM models

--- smoke_app/models.py
+++ smoke_app/models.py
@@ -1,4 +1,33 @@
 from django.db import models
 
+class User(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=255)
+    is_admin = models.BooleanField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.FloatField()
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Order(models.Model):
+    total_price = models.FloatField()
+    status = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, related_name='actions', on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, related_name='requests', on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    order = models.ForeignKey(Order, related_name='details', on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
