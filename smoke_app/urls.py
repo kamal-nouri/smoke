e91dc0cbb7a99f325d22e2e237f5c7b31d99de26
kamal <kamal@email.com>
2020-12-23 13:00:03 +0200

    'maging the log in and regestration page '

--- smoke_app/urls.py
+++ smoke_app/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.root),
+    path('registration',views.registration),
+    path('sign_in',views.sign_in),
+    path('register',views.register),
+    path('success',views.success),
+    path('delete',views.delete),
+    path('log_in',views.log_in),
+    
 ]
