0a03cddedd8e570c0f0b32d4c5ffc08f386ec9b2
Bahaa Hashim <bahaahashim96@gmail.com>
2020-12-23 14:48:32 +0200

    'test pulling'

--- smoke_app/urls.py
+++ smoke_app/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('shisha_cat',views.shisha_cat),
+    path('accessories_cat',views.accessories_cat),
+    path('electronic_cat',views.electronic_cat),
 ]
--- smoke_app/views.py
+++ smoke_app/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render, redirect
 
 def index(request):
-    return render(request, 'category.html')
+    return render(request, 'home.html')
+
+
+def shisha_cat(request):
+    return render(request,'category.html')
+
+def accessories_cat(request):
+    return render(request,'category.html')
+
+def electronic_cat(request):
+    return render(request,'category.html')
+
