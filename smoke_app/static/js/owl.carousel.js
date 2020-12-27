a216e9cff5125b06ecae46bfbfbd4f90d198f5d2
kamal <kamal@email.com>
2020-12-27 10:49:42 +0200

    remove

--- smoke_app/static/js/owl.carousel.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function () {
-  $('.owl-carousel').owlCarousel({
-    items: 5,
-    lazyLoad: true,
-    navigation: true,
-    navigationText: ['<h6 style="margin: 0; padding: 5px 10px">PREV</h6>', '<h6 style="margin: 0; padding: 5px 10px">NEXT</h6>'],
-  });
-});
